a90f9c3d6b9ece3926c4bc60f58b3ba8e59ef1de
idek <1601321@st.asojuku.ac.jp>
2018-01-17 16:31:02 +0900

    tuika

---
+++ project/ModelMgr.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <map>
+#include "GameMain.h"
+
+class ModelMgr
+{
+private:
+	ModelMgr();
+	ModelMgr(const ModelMgr&);
+	static ModelMgr* ptr;
+	
+	std::map<std::string, int> ModelID;					//モデルのハンドル
+	std::map < std::string, SCENE_TYPE> deleteTiming;	//モデルの消去タイミング
+	int materialNum;
+
+public:
+	~ModelMgr();
+
+	static ModelMgr* Instance(void);
+	int ModelIdReturn(const std::string path, const SCENE_TYPE delScene);
+	void SetMaterialDotLine(int modelhandle, float DotLineNum);
+	void ModelManager(const SCENE_TYPE deleScene);
+	void ModelIdAllDelete(void);
+};
+
