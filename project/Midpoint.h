8af308b244bbb252cc2d27ae4138620e5a7918ee
naka <1601345@st.asojuku.ac.jp>
2018-01-26 13:01:52 +0900

    中間点２を追加。ついてくる処理を調節中です

--- project/Midpoint.h
+++ project/Midpoint.h
@@ -12,10 +12,14 @@ private:
 	void GetPoint();	//中間地点にたどり着いたことを示す
 	void FollowDir();	//ついていくための方向などをここで設定
 
-	Position2 _pos;
-	Position2 initPos;
-	Rect _hitRect;
+	Position2 _pos;		//鍵などの場所
+	Position2 _midPos;	//薬品、チェックポイントの場所
+
+	Position2 initPos;	//リトライ時のやり直し地点について
+	Rect _hitRect;		//鍵のあたり判定について
+	Rect _midRect;		//中間点のあたり判定について
 	DIR tmpDir;			//プレイヤーの向いている方向を保持する
+	bool checkpointFlag;//鍵を手に入れたかどうかについて
 	bool GetFlag;		//目的物を取得したかどうかのフラグ
 	//実験
 	bool bubbleFlag;
@@ -29,7 +33,9 @@ public:
 	void GetClass(Player* p);
 	void Draw(Position2 offset);
 	bool ReturnGetFlag();		//中間地点にたどり着いているかいないかを返す
+	bool ReturnCheckFlag();		//鍵を取得しているかどうかを返す
 	Rect& GetRect();
+	Rect& GetRect2();
 	Position2& GetInitPos();
 };
 
