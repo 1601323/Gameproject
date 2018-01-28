ae8f3c7b420c1d02c454195809290d0c8f2f2df0
井手啓輔 <1601321@st.asojuku.ac.jp>
2018-01-28 13:11:55 +0900

    Resultとりま表示だけ

--- project/ResultScene.h
+++ project/ResultScene.h
@@ -2,6 +2,8 @@
 #include "Scene.h"
 
 class Input;
+class ModelMgr;
+
 enum NEXT_MODE {
 	JUMP_RETRY,
 	JUMP_SELECT,
@@ -19,11 +21,26 @@ private:
 	KEY lastKey;
 	INPUT_INFO inpInfo;
 	NEXT_MODE _mode;
-	SENSING_VALUE _minSensingValueL; 
+	SENSING_VALUE _minSensingValueL;
 	NEXT_MODE mode[JUMP_MAX];
 	int nowNum;
+	int bgHandle;
+	int ScoreSelectHandle;
 	bool clearFlag;
 	bool selectFlag;
+
+	ModelMgr* _modelmgr;
+	int playerModelHandle;
+	int smileTexture;
+	int textureIndex;
+	int medicineHandle;
+	float AnimTotalTime;
+	float  AnimNowTime;
+	float outlineNum;
+	int  AnimIndex;
+
+
+
 	void NormalUpdata(Input* input);
 
 	void GameClear();
@@ -36,4 +53,3 @@ public:
 	SCENE_TYPE GetScene();
 	void Updata(Input* input);
 };
-
