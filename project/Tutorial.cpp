a1f8d309ea923e19d13db42dc59e8e3355720b41
naka <1601345@st.asojuku.ac.jp>
2018-02-09 11:12:33 +0900

    応急処置

--- project/Tutorial.cpp
+++ project/Tutorial.cpp
@@ -20,6 +20,8 @@ Tutorial::Tutorial(Player& player, HitClass& h):_player(player),_hit(h)
 	for (int f = 0; f < MESSAGE_NUM; f++) {
 		MessageRect2[f].w = 20;
 		MessageRect2[f].h = 20;
+		HitFlag[f] = false;
+		checkFlag[f] = false;
 	}
 
 }
@@ -34,8 +36,27 @@ void Tutorial::Updata(Input* input)
 	_key = input->GetInput(1).key;
 	_lastKey = input->GetLastKey();
 	_inpInfo = input->GetInput(1);
-	CheckChainPosition();
+	//CheckChainPosition();
 	
+	//if (DrawMessageBoxFlag == false) {
+		for (int f = 0; f < MESSAGE_NUM; f++) {
+			if (_hit.IsHit(GetRect2(f), _player.GetRect()))
+			{
+				messageMoveCheck = true;
+				break;
+			}
+			else {
+				DrawMessageBoxFlag = false;
+			}
+		}
+	//}
+	check1();
+	check2();
+	check3();
+	check4();
+	check5();
+	check6();
+	check7();
 	//面倒なのでflagで
 	if (_inpInfo.num >= 1) {
 		padFlag = true;
@@ -84,50 +105,87 @@ void Tutorial::Draw(Position2 &offset)
 		//MessageRect.SetCenter(drawMassagePosition[i].x, drawMassagePosition[i].y);
 		//MessageRect.Draw(offset);
 		MessageRect2[i].SetCenter(drawMassagePosition[i].x, drawMassagePosition[i].y);
-		//MessageRect2[i].Draw(offset);
+		MessageRect2[i].Draw(offset);
 	}
-
 	if (DrawMessageBoxFlag)
 	{
-		//それぞれのflagがtrueなら描画しますよ
 		//1 移動ジャンプ
-		if (MessageNumFlag[0])
-		{
+		if (MessageNumFlag[0] == true) {
 			DrawGraph(200, 150, im.ImageIdReturn(padFlag ? "image/Tutorial/GamePad/Tutorial1.5.png" : "image/Tutorial/Keyboard/Tutorial1.png", SCENE_RESULT), true);
+			DrawGraph(530, 380, im.ImageIdReturn(padFlag ? "image/UI/backBotton1.png" : "image/UI/keyBackSpace.png", SCENE_RESULT), true);
 		}
 		//2 張り付く
-		else if (MessageNumFlag[1])
-		{
+		if (MessageNumFlag[1] == true) {
 			DrawGraph(200, 10, im.ImageIdReturn(padFlag ? "image/Tutorial/GamePad/Tutorial2.5.png" : "image/Tutorial/Keyboard/Tutorial2.png", SCENE_RESULT), true);
+			DrawGraph(530, 380, im.ImageIdReturn(padFlag ? "image/UI/backBotton1.png" : "image/UI/keyBackSpace.png", SCENE_RESULT), true);
 		}
 		//3 ロープ
-		else if (MessageNumFlag[2])
-		{
+		if (MessageNumFlag[2] == true) {
 			DrawGraph(200, 200, im.ImageIdReturn(padFlag ? "image/Tutorial/GamePad/Tutorial3.5.png" : "image/Tutorial/Keyboard/Tutorial3.png", SCENE_RESULT), true);
+			DrawGraph(530, 380, im.ImageIdReturn(padFlag ? "image/UI/backBotton1.png" : "image/UI/keyBackSpace.png", SCENE_RESULT), true);
 		}
 		//4 ステルス
-		else if (MessageNumFlag[3])
-		{
+		if (MessageNumFlag[3] == true) {
 			DrawGraph(200, 200, im.ImageIdReturn("image/Tutorial/Keyboard/Tutorial4.png", SCENE_RESULT), true);
+			DrawGraph(530, 380, im.ImageIdReturn(padFlag ? "image/UI/backBotton1.png" : "image/UI/keyBackSpace.png", SCENE_RESULT), true);
 		}
 		//5 鍵とか金庫
-		else if (MessageNumFlag[4])
-		{
-			DrawGraph(200, 200, im.ImageIdReturn("image/Tutorial/Keyboard/Tutorial5.png", SCENE_RESULT), true);
+		if (MessageNumFlag[4] == true) {
+			DrawGraph(200, 10, im.ImageIdReturn(padFlag ? "image/Tutorial/GamePad/Tutorial2.5.png" : "image/Tutorial/Keyboard/Tutorial2.png", SCENE_RESULT), true);
+			DrawGraph(530, 380, im.ImageIdReturn(padFlag ? "image/UI/backBotton1.png" : "image/UI/keyBackSpace.png", SCENE_RESULT), true);
 		}
 		//6 トランスアイテム(食パン)
-		else if (MessageNumFlag[5])
-		{
-			DrawGraph(200, 200 ,im.ImageIdReturn("image/Tutorial/Keyboard/Tutorial6.png", SCENE_RESULT), true);
+		if (MessageNumFlag[5] == true) {
+			DrawGraph(200, 200, im.ImageIdReturn("image/Tutorial/Keyboard/Tutorial5.png", SCENE_RESULT), true);
+			DrawGraph(530, 380, im.ImageIdReturn(padFlag ? "image/UI/backBotton1.png" : "image/UI/keyBackSpace.png", SCENE_RESULT), true);
 		}
-		///7 ゴール
-		else if (MessageNumFlag[6])
-		{
-			DrawGraph(560, 450, im.ImageIdReturn("image/Tutorial/Keyboard/Tutorial7.png", SCENE_RESULT), true);
+		//6 トランスアイテム(食パン)
+		if (MessageNumFlag[6] == true) {
+			DrawGraph(200, 200, im.ImageIdReturn("image/Tutorial/Keyboard/Tutorial6.png", SCENE_RESULT), true);
+			DrawGraph(530, 380, im.ImageIdReturn(padFlag ? "image/UI/backBotton1.png" : "image/UI/keyBackSpace.png", SCENE_RESULT), true);
 		}
-
-		DrawGraph(530, 380, im.ImageIdReturn(padFlag ? "image/UI/backBotton1.png" : "image/UI/keyBackSpace.png", SCENE_RESULT), true);
 	}
+	//if (DrawMessageBoxFlag)
+	//{
+	//	//それぞれのflagがtrueなら描画しますよ
+	//	//1 移動ジャンプ
+	//	if (MessageNumFlag[0])
+	//	{
+	//		DrawGraph(200, 150, im.ImageIdReturn(padFlag ? "image/Tutorial/GamePad/Tutorial1.5.png" : "image/Tutorial/Keyboard/Tutorial1.png", SCENE_RESULT), true);
+	//	}
+	//	//2 張り付く
+	//	else if (MessageNumFlag[1])
+	//	{
+	//		DrawGraph(200, 10, im.ImageIdReturn(padFlag ? "image/Tutorial/GamePad/Tutorial2.5.png" : "image/Tutorial/Keyboard/Tutorial2.png", SCENE_RESULT), true);
+	//	}
+	//	//3 ロープ
+	//	else if (MessageNumFlag[2])
+	//	{
+	//		DrawGraph(200, 200, im.ImageIdReturn(padFlag ? "image/Tutorial/GamePad/Tutorial3.5.png" : "image/Tutorial/Keyboard/Tutorial3.png", SCENE_RESULT), true);
+	//	}
+	//	//4 ステルス
+	//	else if (MessageNumFlag[3])
+	//	{
+	//		DrawGraph(200, 200, im.ImageIdReturn("image/Tutorial/Keyboard/Tutorial4.png", SCENE_RESULT), true);
+	//	}
+	//	//5 鍵とか金庫
+	//	else if (MessageNumFlag[4])
+	//	{
+	//		DrawGraph(200, 200, im.ImageIdReturn("image/Tutorial/Keyboard/Tutorial5.png", SCENE_RESULT), true);
+	//	}
+	//	//6 トランスアイテム(食パン)
+	//	else if (MessageNumFlag[5])
+	//	{
+	//		DrawGraph(200, 200 ,im.ImageIdReturn("image/Tutorial/Keyboard/Tutorial6.png", SCENE_RESULT), true);
+	//	}
+	//	///7 ゴール
+	//	else if (MessageNumFlag[6])
+	//	{
+	//		DrawGraph(560, 450, im.ImageIdReturn("image/Tutorial/Keyboard/Tutorial7.png", SCENE_RESULT), true);
+	//	}
+
+	//	DrawGraph(530, 380, im.ImageIdReturn(padFlag ? "image/UI/backBotton1.png" : "image/UI/keyBackSpace.png", SCENE_RESULT), true);
+	//}
 }
 
 void Tutorial::Getclass(HitClass& h, Player& pl)
@@ -165,3 +223,153 @@ TutoList_t& Tutorial::TutorialList()
 	return _tutoList;
 }
 
+void Tutorial::check1()
+{
+	if (_hit.IsHit(GetRect2(0), _player.GetRect()) == true) {
+ 		if (HitFlag[0] == false) {
+			MessageNumFlag[0] = true;
+			DrawMessageBoxFlag = true;
+			checkFlag[0] = true;
+		}
+	}
+	else {
+		MessageNumFlag[0] = false;
+	}
+	if (_key.keybit.B_BUTTON && !_lastKey.keybit.B_BUTTON)
+	{
+		if (checkFlag[0] == true) {
+			DrawMessageBoxFlag = false;
+			HitFlag[0] = true;
+		}
+	}
+}
+void Tutorial::check2()
+{
+	if (_hit.IsHit(GetRect2(1), _player.GetRect()) == true) {
+		if (HitFlag[1] == false) {
+			DrawMessageBoxFlag = true;
+			MessageNumFlag[1] = true;
+			checkFlag[1] = true;
+		}
+	}
+	else {
+		MessageNumFlag[1] = false;
+	}
+
+	if (_key.keybit.B_BUTTON && !_lastKey.keybit.B_BUTTON)
+	{
+		if (checkFlag[1] == true) {
+			DrawMessageBoxFlag = false;
+			HitFlag[1] = true;
+		}
+	}
+
+}
+void Tutorial::check3()
+{
+	if (_hit.IsHit(GetRect2(2), _player.GetRect()) == true) {
+		if (HitFlag[2] == false) {
+			DrawMessageBoxFlag = true;
+			MessageNumFlag[2] = true;
+			checkFlag[2] = true;
+		}
+	}
+	else {
+		MessageNumFlag[2] = false;
+	}
+
+	if (_key.keybit.B_BUTTON && !_lastKey.keybit.B_BUTTON)
+	{
+		if (checkFlag[2] == true) {
+			DrawMessageBoxFlag = false;
+			HitFlag[2] = true;
+		}
+	}
+}
+void Tutorial::check4()
+{
+	if (_hit.IsHit(GetRect2(3), _player.GetRect()) == true) {
+		if (HitFlag[3] == false) {
+			DrawMessageBoxFlag = true;
+			MessageNumFlag[3] = true;
+			checkFlag[3] = true;
+		}
+	}
+	else {
+		MessageNumFlag[3] = false;
+	}
+
+	if (_key.keybit.B_BUTTON && !_lastKey.keybit.B_BUTTON)
+	{
+		if (checkFlag[3] == true) {
+			DrawMessageBoxFlag = false;
+			HitFlag[3] = true;
+		}
+	}
+
+}
+void Tutorial::check5()
+{
+	if (_hit.IsHit(GetRect2(4), _player.GetRect()) == true) {
+		if (HitFlag[4] == false) {
+			DrawMessageBoxFlag = true;
+			MessageNumFlag[4] = true;
+			checkFlag[4] = true;
+		}
+	}
+	else {
+		MessageNumFlag[4] = false;
+	}
+
+	if (_key.keybit.B_BUTTON && !_lastKey.keybit.B_BUTTON)
+	{
+		if (checkFlag[4] == true) {
+			DrawMessageBoxFlag = false;
+			HitFlag[4] = true;
+		}
+	}
+
+}
+void Tutorial::check6()
+{
+	if (_hit.IsHit(GetRect2(5), _player.GetRect()) == true) {
+		if (HitFlag[5] == false) {
+			DrawMessageBoxFlag = true;
+			MessageNumFlag[5] = true;
+			checkFlag[5] = true;
+		}
+	}
+	else {
+		MessageNumFlag[5] = false;
+	}
+
+	if (_key.keybit.B_BUTTON && !_lastKey.keybit.B_BUTTON)
+	{
+		if (checkFlag[5] == true) {
+			DrawMessageBoxFlag = false;
+			HitFlag[5] = true;
+		}
+	}
+}
+void Tutorial::check7()
+{
+	if (_hit.IsHit(GetRect2(6), _player.GetRect()) == true) {
+		if (HitFlag[6] == false) {
+			DrawMessageBoxFlag = true;
+			MessageNumFlag[6] = true;
+			checkFlag[6] = true;
+		}
+	}
+	else {
+		MessageNumFlag[6] = false;
+	}
+
+	if (_key.keybit.B_BUTTON && !_lastKey.keybit.B_BUTTON)
+	{
+		if (checkFlag[6] == true) {
+			DrawMessageBoxFlag = false;
+			HitFlag[6] = true;
+		}
+	}
+
+}
